9bed36d60df9be797a6fdf257e3dbb20199b49d9
Cyberdog90 <43635561+Cyberdog90@users.noreply.github.com>
2020-01-11 16:44:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,27 @@ def main():
 
 
 class TextEditor:
-    current_open_file = "no_file"
-
     def __init__(self, master):
-        self.master = master
-        self.font_size = 18
-
         master.geometry("1280x720")
         master.title("PiNE")
         master.iconbitmap("icon.ico")
 
-        self.text_field = tk.Text(font=("MS明朝", self.font_size))
+        self.current_open_file = None
+
+        self.master = master
+        self.font = "MS明朝"
+        self.font_size = 18
+
+        self.text_field = tk.Text(font=(self.font, self.font_size))
         self.text_field.pack(fill=tk.BOTH, expand=1)
-        
+
         self.main_menu = tk.Menu()
         self.master.config(menu=self.main_menu)
 
         self.file_menu = tk.Menu(self.main_menu)
         self.main_menu.add_cascade(label="ファイル", menu=self.file_menu)
         self.file_menu.add_command(label="開く", command=self.open_file)
+        self.file_menu.add_command(label="上書き保存", command=self.overwrite)
         self.file_menu.add_command(label="名前を付けて保存",
                                    command=self.save_file)
 
@@ -40,6 +42,14 @@ class TextEditor:
         self.main_menu.add_cascade(label="書式", menu=self.format_menu)
         self.format_menu.add_command(label="フォントサイズ",
                                      command=self.font_size_increase)
+
+    def overwrite(self):
+        if self.current_open_file is not None:
+            with open(self.current_open_file, "w") as f:
+                text2save = self.text_field.get(1.0, tk.END)
+                f.write(text2save)
+        else:
+            self.save_file()
 
     def open_file(self):
         file_name = fd.askopenfilename(initialdir=os.getcwd(),
@@ -56,18 +66,19 @@ class TextEditor:
                 for i in file:
                     self.text_field.insert(tk.END, i)
 
+        self.current_open_file = file_name
+
     def save_file(self):
-        f = fd.asksaveasfile(initialdir=os.getcwd(),
-                             mode="w",
-                             filetypes=(("テキスト文書", "*.txt"),
-                                        ("すべてのファイル", "*.*")))
+        with fd.asksaveasfile(initialdir=os.getcwd(),
+                              mode="w",
+                              defaultextension=".txt",
+                              filetypes=(("テキスト文書", "*.txt"),
+                                         ("すべてのファイル", "*.*"))) as f:
+            if f is None:
+                return
 
-        if f is None:
-            return
-
-        text2save = self.text_field.get(1.0, tk.END)
-        f.write(text2save)
-        f.close()
+            text2save = self.text_field.get(1.0, tk.END)
+            f.write(text2save)
 
     def font_size_increase(self):
         self.font_size = 49
