1f1a2bde7924d3bed277833ad2ea61a4e7ce6206
Cyberdog90 <43635561+Cyberdog90@users.noreply.github.com>
2020-01-21 15:05:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,39 +12,62 @@ def main():
 
 class TextEditor:
     def __init__(self, master):
+        # window init
         master.geometry("1280x720")
         master.title("Ahoj Editor")
         master.iconbitmap("./data/icon.ico")
+        self.master = master
 
+        # flag variable
         self.current_open_file = None
         self.flag = True
         self.color = True
         self.emergency_call = True
         self.em_text = ""
 
-        self.master = master
+        # font init
         self.font = "MS明朝"
         self.font_size = 18
 
+        # Entry
         self.text_field = tk.Text(font=(self.font, self.font_size), undo=True)
         self.text_field.pack(fill=tk.BOTH, expand=1)
 
+        # menu
         self.main_menu = tk.Menu()
         self.master.config(menu=self.main_menu)
-
         self.file_menu = tk.Menu(self.main_menu, tearoff=False)
 
+        # menu function
         self.file_menu.bind_all("<Control-o>", self.open_file)
+        self.file_menu.bind_all("<Shift-Control-O>", self.open_latest_file)
         self.file_menu.bind_all("<Shift-Control-S>", self.save_file)
         self.file_menu.bind_all("<Control-s>", self.overwrite)
-        self.file_menu.bind_all("<Shift-Control-O>", self.open_latest_file)
         self.file_menu.bind_all("<Escape>", self.quit)
-        self.file_menu.bind_all("<Shift-Control-D>", self.copy_line)
+        self.file_menu.bind_all("<Shift-Control-C>", self.copy_line)
+        self.file_menu.bind_all("<Shift-Control-D>", self.delete_line)
         self.file_menu.bind_all("<Shift-Control-N>", self.night_mode)
-        self.file_menu.bind_all("<Shift-Control-C>", self.command_mode)
+        self.file_menu.bind_all("<Shift-Control-M>", self.command_mode)
         self.file_menu.bind_all("<Shift-Control-E>", self.emergency)
         self.file_menu.bind_all("<Control-semicolon>", self.increment)
         self.file_menu.bind_all("<Control-minus>", self.decrement)
+
+    # bound function
+    def open_file(self, *_):
+        self.open_check()
+        file_name = fd.askopenfilename(initialdir=os.getcwd(),
+                                       title="開く",
+                                       filetypes=(("テキスト文書", "*.txt"),
+                                                  ("すべてのファイル", "*.*")))
+        self.file_open(file_name=file_name)
+        with open("log.txt", "w") as f:
+            f.write(self.current_open_file)
+
+    def open_latest_file(self, *_):
+        self.open_check()
+        with open("log.txt", "r") as f:
+            file_name = f.readline()
+            self.file_open(file_name=file_name)
 
     def night_mode(self, *_):
         if self.color:
@@ -62,30 +85,12 @@ class TextEditor:
         self.open_check()
         self.destroy()
 
+    def delete_line(self, *_):
+        self.text_field.delete("insert linestart", "insert lineend")
+
     def copy_line(self, *_):
-        text = self.text_field.get("insert linestart", "insert lineend")
+        text = self.get_text()
         self.text_field.insert("insert linestart", "{}\n".format(text))
-
-    def open_check(self):
-        text = len(list("".join(self.text_field.get(1.0, tk.END))))
-        if text != 1:
-            self.overwrite()
-        self.text_field.delete(1.0, tk.END)
-
-    def open_latest_file(self, *_):
-        self.open_check()
-        with open("log.txt", "r") as f:
-            file_name = f.readline()
-            try:
-                with open(file_name, encoding="UTF-8") as file:
-                    for i in file:
-                        self.text_field.insert(tk.END, i)
-            except UnicodeDecodeError:
-                with open(file_name) as file:
-                    for i in file:
-                        self.text_field.insert(tk.END, i)
-
-            self.current_open_file = file_name
 
     def overwrite(self, *_):
         if self.current_open_file is not None:
@@ -94,26 +99,6 @@ class TextEditor:
                 f.write(text2save)
         else:
             self.save_file()
-
-    def open_file(self, *_):
-        # self.open_check()
-        file_name = fd.askopenfilename(initialdir=os.getcwd(),
-                                       title="開く",
-                                       filetypes=(("テキスト文書", "*.txt"),
-                                                  ("すべてのファイル", "*.*")))
-
-        try:
-            with open(file_name, encoding="UTF-8") as file:
-                for i in file:
-                    self.text_field.insert(tk.END, i)
-        except UnicodeDecodeError:
-            with open(file_name) as file:
-                for i in file:
-                    self.text_field.insert(tk.END, i)
-
-        self.current_open_file = file_name
-        with open("log.txt", "w") as f:
-            f.write(self.current_open_file)
 
     def save_file(self, *_):
         with fd.asksaveasfile(initialdir=os.getcwd(),
@@ -152,6 +137,27 @@ class TextEditor:
 
     def command_mode(self, *_):
         pass
+
+    # util function
+    def get_text(self):
+        return self.text_field.get("insert linestart", "insert lineend")
+
+    def open_check(self):
+        text = len(list("".join(self.text_field.get(1.0, tk.END))))
+        if text != 1:
+            self.overwrite()
+        self.text_field.delete(1.0, tk.END)
+
+    def file_open(self, file_name):
+        try:
+            with open(file_name, encoding="UTF-8") as file:
+                for i in file:
+                    self.text_field.insert(tk.END, i)
+        except UnicodeDecodeError:
+            with open(file_name) as file:
+                for i in file:
+                    self.text_field.insert(tk.END, i)
+        self.current_open_file = file_name
 
 
 if __name__ == "__main__":
